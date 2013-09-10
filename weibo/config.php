8cbd076fa15ab4b435a92c78294a02eea1d8838a
SH <cctv_243028755@live.cn>
2013-09-10 20:31:39 +0800

    Create config.php

---
+++ weibo/config.php
@@ -0,0 +1,32 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+ define( "WB_AKEY" , '112447102' );
+ define( "WB_SKEY" , '61261417951edf6818aa2e3d2aeff342' );
+ define( "WB_CALLBACK_URL" , 'http://phpoop.sinaapp.com/weibo/callback.php' );
+//  define( "WB_CALLBACK_URL" , 'http://phpoop.sinaapp.com/application/views/load/callback.php' );
+/*申请到的appid*/
+// $config["WB_AKEY"]    = "112447102";
+//$config["WB_AKEY"]    = "3928777956";
+/*$config["qq_appid"]    = "";*/
+/*申请到的appkey*/
+// $config["WB_SKEY"]   = "61261417951edf6818aa2e3d2aeff342";
+
+//$config["WB_SKEY"]   = "b736ea35cb131c6aca3aa6a5e5db1855";
+/*$config["qq_appkey"]   = "";*/
+/*QQ登录成功后跳转的地址,请确保地址真实可用，否则会导致登录失败。*/
+//$config["qq_callback"] = "http://www.phpoop.sinaapp.com/oauth/qqcallback";
+// $config["WB_CALLBACK_URL"] = 'http://phpoop.sinaapp.com/weibo/callback.php';
+
+// $config["WB_SKEY"]   = "b736ea35cb131c6aca3aa6a5e5db1855";
+/*$config["qq_appkey"]   = "";*/
+/*QQ登录成功后跳转的地址,请确保地址真实可用，否则会导致登录失败。*/
+//$config["qq_callback"] = "http://www.phpoop.sinaapp.com/oauth/qqcallback";
+ //$config["WB_CALLBACK_URL"] = 'http://phpoop.sinaapp.com/weibo/callback.php';
+
+// $config["WB_CALLBACK_URL"] = 'http://127.0.0.1/weibo/callback.php';
+
+// $config["WB_CALLBACK_URL"] = 'http://phpoop.sinaapp.com/application/views/load/callback.php';
+
+// $config["WB_CALLBACK_URL"] = 'http://phpoop.sinaapp.com/application/views/load/callback.php';
+
