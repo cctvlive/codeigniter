17fd88ac028fccae0ce0cdbcb24a5ba8cc1e7b7c
SH <cctv_243028755@live.cn>
2013-09-10 20:45:43 +0800

    放置按钮

---
+++ application/view/load/load.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+    <html lang="en" xmlns:wb="http://open.weibo.com/wb">
+  <head>
+    <meta charset="utf-8">
+    <title>&reg;</title>
+      
+    <meta property="qc:admins" content="207007055077063161100637574714717"  >
+     
+    <meta name="description" content="行货">
+    <meta name="author" content="干货">
+    <base href="<?=base_url()?>"/>
+
+      <script src="http://tjs.sjs.sinajs.cn/open/api/js/wb.js?appkey=2876661526" type="text/javascript" charset="utf-8"></script>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="//cdnjs.bootcss.com/ajax/libs/html5shiv/3.6.2/html5shiv.js"></script>
+    <![endif]-->
+    <!-- Fav and touch icons -->                            
+  
+  </head>
+  <body> 
+
+<!-- Masthead
+================================================== -->
+<header class="jumbotron subhead navbar navbar-inverse " id="overview">
+
+          <a href="<?php echo site_url("load/qqlogin")?>" target="_self"><img src="<?php echo base_url("public/images/logo7.png");?>" /></a>||<a id="wb_connect_btn"></a>
+
+<p><a href="<?php echo site_url('load/sin_login');?>"><img src="./style/img/weibo_login.png" title="点击进入授权页面" alt="点击进入授权页面" border="0" /></a></p>
+        
